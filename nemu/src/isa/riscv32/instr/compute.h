a7f16b1387e2674ec59cdef8111babd9bebee7b7
logic <925663486@qq.com>
2022-08-15 14:47:08 +0800

    PA2.1 finish
    use 8h
    add addi  auipc  jal  jalr

--- nemu/src/isa/riscv32/instr/compute.h
+++ nemu/src/isa/riscv32/instr/compute.h
@@ -1,3 +1,24 @@
 def_EHelper(lui) {
   rtl_li(s, ddest, id_src1->imm);
 }
+def_EHelper(auipc) {
+  rtl_li(s, ddest, id_src1->imm);
+  rtl_addi(s, ddest, ddest,cpu.pc);
+}
+def_EHelper(addi) {
+  rtl_addi(s, ddest, dsrc1,id_src2->imm);
+}
+
+def_EHelper(jal) {
+  rtl_addi(s, ddest, &cpu.pc,id_src1->imm);
+  rtl_jr(s,ddest);
+  rtl_addi(s, ddest, &cpu.pc,4);
+}
+def_EHelper(jalr) {
+  rtl_addi(s, s0, &cpu.pc,4);
+
+  rtl_addi(s, ddest, dsrc1, id_src2->imm);
+  rtl_andi(s,ddest,ddest,~1);
+  rtl_jr(s,ddest);
+  rtl_mv(s, ddest, s0);
+}
