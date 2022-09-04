4d3ea26fd515f62f8a7f45853824fd478f0b3d17
logic <925663486@qq.com>
2022-09-04 15:16:16 +0800

    finish keyboard

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -1,9 +1,20 @@
 #include <am.h>
 #include <nemu.h>
-
+#include <stdio.h>
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
+  uint32_t keycode = inl(KBD_ADDR);
+  if (keycode > KEYDOWN_MASK) {
+    kbd->keydown = 1;
+    kbd->keycode = keycode - KEYDOWN_MASK;
+    return;
+  } else {
+    kbd->keydown = 0;
+    kbd->keycode = keycode;
+    return;
+  }
+
   kbd->keydown = 0;
   kbd->keycode = AM_KEY_NONE;
 }
--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -81,18 +81,24 @@ int atoi(const char* nptr) {
   }
   return x;
 }
+static unsigned long int addr = -1;
 
 void *malloc(size_t size) {
   // On native, malloc() will be called during initializaion of C runtime.
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
-#if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-  panic("Not implemented");
-#endif
-  return NULL;
+
+// #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
+//   panic("Not implemented");
+// #endif
+//   return NULL;
+  if(addr==-1)addr = (unsigned long int)heap.start;
+  addr+=size;
+  return (void *)addr-size;
 }
 
 void free(void *ptr) {
+  return;
 }
 
 #endif
