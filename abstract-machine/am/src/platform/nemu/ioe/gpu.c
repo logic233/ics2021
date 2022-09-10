444772539ad48ab45758571a43baa266910446d2
logic <925663486@qq.com>
2022-09-10 20:40:32 +0800

    add gpu

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -1,20 +1,48 @@
 #include <am.h>
 #include <nemu.h>
-
+#include <stdio.h>
+#include <string.h>
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
+  // int i;
+  // int w = inl(VGACTL_ADDR)>>16;  // TODO: get the correct width
+  // int h = inl(VGACTL_ADDR)&0xFFFF;  // TODO: get the correct height
+  // uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  // // for (i = 0; i < w * h; i++) fb[i] = i;
+  // outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
+  uint32_t width_height = inl(VGACTL_ADDR);
+  // printf("$$$$%d %d",width_height&0xFFFF,width_height>>16);
+
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = 0, .height = 0,
+    .width = width_height>>16, .height = width_height&0xFFFF,
     .vmemsz = 0
   };
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+
+  int ww = inl(VGACTL_ADDR)>>16;  
+  // int hh = inl(VGACTL_ADDR)&0xFFFF;  
+// int x, y; void *pixels; int w, h; bool sync
+  int x= ctl->x;
+  int y= ctl->y;
+  uint32_t *pixels = (uint32_t *)ctl->pixels;
+  int w= ctl->w;
+  int h =ctl->h;
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+
+  for(int i=0;i<h;i++){
+    for(int j=0;j<w;j++){
+      fb[(i+y)*ww+x+j]=pixels[i*w+j];
+    }
+    // strncpy(fb+(i+y)*ww+x,pixels+i*w,w);
+  }
+
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -18,7 +18,6 @@ static uint32_t screen_size() {
 
 static void *vmem = NULL;
 static uint32_t *vgactl_port_base = NULL;
-
 #ifdef CONFIG_VGA_SHOW_SCREEN
 #ifndef CONFIG_TARGET_AM
 #include <SDL2/SDL.h>
@@ -57,12 +56,17 @@ static inline void update_screen() {
 
 void vga_update_screen() {
   // TODO: call `update_screen()` when the sync register is non-zero,
-  // then zero out the sync register
+  // then zero out the sync register  
+  if(vgactl_port_base[1]!=0){
+    update_screen();
+    vgactl_port_base[1]=0;
+  }
 }
 
 void init_vga() {
   vgactl_port_base = (uint32_t *)new_space(8);
   vgactl_port_base[0] = (screen_width() << 16) | screen_height();
+  vgactl_port_base[1] = 0;
 #ifdef CONFIG_HAS_PORT_IO
   add_pio_map ("vgactl", CONFIG_VGA_CTL_PORT, vgactl_port_base, 8, NULL);
 #else
